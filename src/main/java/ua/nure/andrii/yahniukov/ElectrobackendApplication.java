85c0d42be8d777e7ff4d5cd11ccf9f46cfe0a33c
Andrii Yahniukov <andrii.yahniukov@gmail.com>
2022-05-31 11:09:34 +0300

    first commit

---
+++ src/main/java/ua/nure/andrii/yahniukov/ElectrobackendApplication.java
@@ -0,0 +1,13 @@
+package ua.nure.andrii.yahniukov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectrobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectrobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/nure/andrii/yahniukov/ElectrobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nure.andrii.yahniukov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectrobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
